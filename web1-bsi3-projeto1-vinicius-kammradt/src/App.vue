e3445061b410a2c30ba674a506c13c39a4bf899a
kammradt <vinicius.kammradt1@gmail.com>
2019-03-29 07:55:00 -0300

    UPDATE: Template done.

---
+++ web1-bsi3-projeto1-vinicius-kammradt/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vinicius</span>
+        <span class="font-weight-light">Kammradt</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        small
+        href="https://github.com/kammradt/faculdade-webdev"
+        target="_blank"
+      >
+        <v-icon left>
+          code
+        </v-icon>
+        <span class="mr-2">Github</span>
+      </v-btn>
+    </v-toolbar>
+
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
