eb17c5fc31871cbf94670eaf978977d132baa22e
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-04-01 09:54:57 -0300

    funcionando

--- web1-bsi3-projeto1-vinicius-kammradt/pages/_id.vue
+++ web1-bsi3-projeto1-vinicius-kammradt/pages/_id.vue
@@ -3,16 +3,16 @@
     <VContent>
       <h1>
         <!--Aqui é o indice da noticia que o cara clicou, /2 por ex -->
-        {{ $nuxt.$route.path.substring(1) }}
+        {{ $route.params.id }}
       </h1>
       <div>
         <!--Aqui é a notícia usando o indice -->
-        {{ news[$nuxt.$route.path.substring(1)] }}
+        {{ news }}
       </div>
       <div>
         <!--Aqui é o titulo da noticia que o cara clicou -->
         <!--Ele mostrar o título uma vez, e depois se atualizar a pagina da pau-->
-        <!-- {{ news[$nuxt.$route.path.substring(1)].title }} -->
+        {{ news.title }}
       </div>
     </VContent>
   </VApp>
@@ -21,15 +21,16 @@
 <script>
 import axios from 'axios'
 export default {
-  data() {
-    return {
-      news: {}
-    }
-  },
-  mounted() {
-    axios
-      .get('https://newsapi.org/v2/top-headlines?country=br&apiKey=df575fbf5083481f828502affa15caf6')
-      .then(response => (this.news = response.data.articles))
+  asyncData({ app, route }) {
+    console.log(route.params.id)
+    return Promise.all([
+      axios.get('https://newsapi.org/v2/top-headlines?country=br&apiKey=df575fbf5083481f828502affa15caf6')
+    ])
+      .then((response) => {
+        return {
+          news: response[0].data.articles[route.params.id]
+        }
+      })
   }
 }
 </script>
