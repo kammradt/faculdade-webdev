a028e0986c876b1959d54ecc8551125a9fb33e13
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-04-01 09:57:43 -0300

    Apenas mostrando o título

--- web1-bsi3-projeto1-vinicius-kammradt/pages/_id.vue
+++ web1-bsi3-projeto1-vinicius-kammradt/pages/_id.vue
@@ -2,18 +2,8 @@
   <VApp>
     <VContent>
       <h1>
-        <!--Aqui é o indice da noticia que o cara clicou, /2 por ex -->
-        {{ $route.params.id }}
-      </h1>
-      <div>
-        <!--Aqui é a notícia usando o indice -->
-        {{ news }}
-      </div>
-      <div>
-        <!--Aqui é o titulo da noticia que o cara clicou -->
-        <!--Ele mostrar o título uma vez, e depois se atualizar a pagina da pau-->
         {{ news.title }}
-      </div>
+      </h1>
     </VContent>
   </VApp>
 </template>
@@ -22,7 +12,6 @@
 import axios from 'axios'
 export default {
   asyncData({ app, route }) {
-    console.log(route.params.id)
     return Promise.all([
       axios.get('https://newsapi.org/v2/top-headlines?country=br&apiKey=df575fbf5083481f828502affa15caf6')
     ])
