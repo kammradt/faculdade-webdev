8d6855923cc5bcf9fbd3c51cc9c67b107b93271b
kammradt <vinicius.kammradt1@gmail.com>
2019-04-12 20:46:47 -0300

    UPDATE: Fixing folders.

--- bkp/src/components/MyFooter.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-footer class="primary pa-3" dark absolute>
-    <v-spacer></v-spacer>
-    <div class="font-weight-bold">
-      Vue.js | Vuetify | &copy; {{ new Date().getFullYear() }}
-    </div>
-  </v-footer>
-</template>
